bc7551cb00a1838be6bb6abc2754d8ca6ab788dc
NhanVo-gmt <tonynhanvo@gmail.com>
2024-02-16 21:58:45 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import "./App.css"
+import Header from './components/common/header/header'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Router>
+        <Header />
+        <div>
+
+          <Switch>
+            {/* <Route path="/" component={Homepages} /> */}
+          </Switch>
+        </div>
+      </Router>
+    </>
+  )
 }
 
-export default App;
+export default App
