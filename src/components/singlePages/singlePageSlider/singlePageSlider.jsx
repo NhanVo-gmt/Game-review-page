3036fb8e0e590157ea3f4e4afa40c4136431bd7c
NhanVoo <141220487+NhanVoo@users.noreply.github.com>
2024-02-21 01:12:57 +1100

    adding slider and single page

--- src/components/singlePages/singlePageSlider/singlePageSlider.jsx
+++ src/components/singlePages/singlePageSlider/singlePageSlider.jsx
@@ -1,11 +1,51 @@
-import React from 'react'
+import React from "react";
+import Slider from "react-slick";
+import Heading from "../../common/heading/heading";
+import { popular } from "../../../dummydata";
+import "./singlePageSlider.css"
 
 const SinglePageSlider = () => {
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 7,
+    slidesToScroll: 2,
+    responsive: [
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
   return (
     <>
-      
+      <section className="singlePopular">
+        <Slider {...settings}>
+          {popular.map((val) => {
+            return (
+              <div className="items">
+                <div className="box">
+                  <div className="images">
+                    <div className="img">
+                      <img src={val.cover} alt="" />
+                    </div>
+                  </div>
+                  <div className="text">
+                    <h1 className="title">{val.title}</h1>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </Slider>
+      </section>
     </>
-  )
-}
+  );
+};
 
-export default SinglePageSlider
+export default SinglePageSlider;
